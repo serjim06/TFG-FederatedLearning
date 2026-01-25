34fda47dfaaaa442bb9f9f8b2fcec599897cf981
serjim06 <serjim06@ucm.es>
2026-01-25 14:08:08 +0100

    feat(user_info, user_list): Añadir la lista de usuarios y eliminar usuario

---
+++ src/gui/user_info.py
@@ -0,0 +1,95 @@
+import json
+from sqlite3 import DatabaseError
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import uuid
+
+import src.db.dbcon as dbcon
+from src.utils import utils
+
+class UserInfoDialog(tk.Toplevel):
+    def __init__(self, parent, user_data):
+        super().__init__(parent)
+        self.title("User Information")
+        self.geometry("400x600")
+        self.resizable(False, False)
+        
+        self.configure(bg="#eef4fb")
+        
+        utils.get_style()
+        
+        self.transient(parent)
+        self.grab_set()
+        self.focus_set()
+        
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
+        
+        self.user_data = user_data
+        print(user_data["id"])
+        self.parent = parent
+        
+        self.top_frame = tk.Frame(self)
+        self.top_frame.pack(fill="both", expand=True)
+        
+        self.canvas = tk.Canvas(self.top_frame, borderwidth=0, background="#eef4fb")
+        self.frame = tk.Frame(self.canvas, background="#eef4fb")
+        self.vsb = tk.Scrollbar(self.top_frame, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+        self.frame.bind("<Configure>", self._on_frame_configure)
+
+
+        self.inner_window = self.canvas.create_window((0,0), window=self.frame, anchor="nw")
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        
+                
+        self.id_label = tk.Label(self.frame, text=f"User ID: {str(uuid.UUID(bytes=user_data['id']))}", font=("Arial", 10), bg="#eef4fb")
+        self.id_label.pack(anchor="w", pady=5)
+        
+        self.name_label = tk.Label(self.frame, text=f"Nombre: {user_data['username']}", font=("Arial", 10), bg="#eef4fb")
+        self.name_label.pack(anchor="w", pady=10)
+        
+        
+        self.role_label = tk.Label(self.frame, text=f"Role: {user_data['role']}", font=("Arial", 10), bg="#eef4fb")
+        self.role_label.pack(anchor="w", pady=5)
+        
+        self.user_projects = dbcon.command("select", "projects", {"uid": user_data["id"]})
+        
+        self.projects_label = tk.Label(self.frame, text="Projects:", font=("Arial", 10, "bold"), bg="#eef4fb")
+        self.projects_label.pack(anchor="w", pady=10)
+        
+        for project in self.user_projects:
+            project_frame = tk.Frame(self.frame, bg="#eef4fb", bd=1, relief="solid")
+            project_label = tk.Label(project_frame, text=f"- {project['name']}", font=("Arial", 10), bg="#eef4fb")
+            project_label.pack(anchor="w", padx=20)
+            nodes_frame = tk.Frame(project_frame, bg="#eef4fb", bd=1, relief="solid")
+            nodes = json.loads(project['nodes'])
+            tk.Label(nodes_frame, text="  Nodes:", font=("Arial", 10, "bold"), bg="#eef4fb").pack(anchor="w", padx=30)
+            for node in nodes:
+                tk.Label(nodes_frame, text=f"  - {node}", font=("Arial", 10), bg="#eef4fb").pack(anchor="w", padx=40)
+            nodes_frame.pack(fill="both", expand=True, padx=10, pady=5)
+            project_frame.pack(fill="both", expand=True, padx=10, pady=5)
+            
+        self.buttons_frame = tk.Frame(self)
+        self.buttons_frame.pack(pady=10)
+        
+        ttk.Button(self.buttons_frame, text="Volver", command=self.destroy, style="Sec.TButton").pack(side="left", padx=5)
+        ttk.Button(self.buttons_frame, text="Eliminar Usuario", command=self.delete_user, style="Accent.TButton").pack(side="left", padx=5)
+        
+                
+    def delete_user(self):
+        if messagebox.askyesno("Confirmar Eliminación", "¿Estás seguro de que deseas eliminar este usuario? Esta acción no se puede deshacer. Se eliminarán todos los proyectos asociados a este usuario."):
+            try:
+                dbcon.command("delete", "projects", {"uid": self.user_data["id"]})
+                dbcon.command("delete", "users", {"id": self.user_data["id"]})
+            except (ValueError, DatabaseError) as e:
+                messagebox.showerror("Error", str(e))
+                return
+            
+            messagebox.showinfo("Usuario Eliminado", "El usuario ha sido eliminado correctamente.")
+            self.destroy()
+            
+            
+    def _on_frame_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
--- src/gui/user_list.py
+++ src/gui/user_list.py
@@ -3,13 +3,59 @@ import uuid
 
 from src.db import dbcon
 from src.utils.user import User
+from src.gui.user_info import UserInfoDialog
 
+class UserListFrame(tk.Frame):
+    def __init__(self, parent, switch_frame, usuario : User):
+        super().__init__(parent)
+        self.switch_frame = switch_frame
+        self.user = usuario.to_dict()
+        
+        self.canvas = tk.Canvas(self, borderwidth=0, background="#f0f0f0")
+        self.frame = tk.Frame(self.canvas, background="#f0f0f0")
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.inner_window = self.canvas.create_window((0,0), window=self.frame, anchor="nw")
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.canvas.bind("<Configure>", self._on_canvas_configure)
+
+
+        self.populate()
+        
+    def populate(self):
+        self.clear_users()
+        self.users = dbcon.command("select", "users", {"id": "*"})
+        for user in self.users:
+            real_user = User(**user)
+            if real_user.id == self.user["id"]:
+                continue
+            card = UserCard(self.frame, real_user.to_dict(), self)
+            card.pack(fill="x", pady=5, expand=True)
+            
+            
+    def clear_users(self):
+        for w in self.frame.winfo_children():
+            w.destroy()
+        
+    def onFrameConfigure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        
+    def _on_canvas_configure(self, event):
+        scrollbar_width = self.vsb.winfo_width()
+        self.canvas.itemconfigure(
+            self.inner_window,
+            width=event.width - scrollbar_width
+        )
 
 class UserCard(tk.Frame):
-    def __init__(self, parent, user_data):
+    def __init__(self, parent : UserListFrame, user_data, user_list_frame):
         super().__init__(parent, bg="#eeeeee", bd=1, relief="solid", padx=10, pady=10)
         
-        self.parent = parent
+        self.user_list_frame = user_list_frame
         
         self.user_data = user_data
         
@@ -49,57 +95,7 @@ class UserCard(tk.Frame):
             w.configure(bg="#eeeeee")
             
     def _on_click(self, event):
-        UserInfoDialog(self.parent, self.user_data)
+        UserInfoDialog(self.user_list_frame, self.user_data)
+    
         
-class UserInfoDialog(tk.Toplevel):
-    def __init__(self, parent, user_data):
-        super().__init__(parent)
-        self.title("User Information")
-        self.geometry("300x200")
-        self.resizable(False, False)
-        
-        
-        tk.Button(self, text="Close", command=self.destroy).pack(pady=10)
-        
-        
-class UserListFrame(tk.Frame):
-    def __init__(self, parent, switch_frame, usuario : User):
-        super().__init__(parent)
-        self.switch_frame = switch_frame
-        self.user = usuario.to_dict()
-
-        self.users = dbcon.command("select", "users", {"id": "*"})
-        
-        self.canvas = tk.Canvas(self, borderwidth=0, background="#f0f0f0")
-        self.frame = tk.Frame(self.canvas, background="#f0f0f0")
-        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=self.vsb.set)
-
-        self.inner_window = self.canvas.create_window((0,0), window=self.frame, anchor="nw")
-        self.vsb.pack(side="right", fill="y")
-        self.canvas.pack(side="left", fill="both", expand=True)
-
-        self.frame.bind("<Configure>", self.onFrameConfigure)
-        self.canvas.bind("<Configure>", self._on_canvas_configure)
-
-
-        self.populate()
-        
-    def populate(self):
-        for user in self.users:
-            real_user = User(**user)
-            if real_user.id == self.user["id"]:
-                continue
-            card = UserCard(self.frame, real_user.to_dict())
-            card.pack(fill="x", pady=5, expand=True)
-            
-    def onFrameConfigure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-        
-    def _on_canvas_configure(self, event):
-        scrollbar_width = self.vsb.winfo_width()
-        self.canvas.itemconfigure(
-            self.inner_window,
-            width=event.width - scrollbar_width
-        )
 
